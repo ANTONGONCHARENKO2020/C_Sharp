b371ed16825ea5fdec786d1907d7f5afc55c6c04
Anton <gonchar.1994@mail.ru>
2022-11-20 20:31:54 +0300

    удалил 17 строку

--- DZ02/Program.cs
+++ DZ02/Program.cs
@@ -14,7 +14,6 @@ int number3 = new int();
 Console.WriteLine("Введите число");
 number3 = Convert.ToInt32(Console.ReadLine());
 int max = number1;
-if(number1 > max) max = number1;
 if(number2 > max) max = number2;
 if(number3 > max) max = number3;
 Console.WriteLine("Большее число");
