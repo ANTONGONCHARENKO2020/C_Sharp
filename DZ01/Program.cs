abe7d6671e98c11da3d785d3c92f1992f965ebd6
Anton <gonchar.1994@mail.ru>
2022-11-20 20:53:56 +0300

    Написал программу

--- DZ01/Program.cs
+++ DZ01/Program.cs
@@ -1,1 +1,15 @@
-﻿int Number1 = new int();
+﻿int number1 = new int();
+Console.WriteLine("Введите число");
+number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = new int();
+Console.WriteLine("Введите число");
+number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Большее число");
+if(number1 < number2)
+{
+    Console.WriteLine(number2);
+}
+else
+{
+    Console.WriteLine(number1);
+}
